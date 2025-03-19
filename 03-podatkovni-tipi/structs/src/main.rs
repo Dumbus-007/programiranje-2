865bd169e0417f9a127202df79f4f2694ba1f253
Lara Velkavrh <lv31155@student.uni-lj.si>
2025-03-19 09:54:30 +0100

    četrte vaje

--- 03-podatkovni-tipi/structs/src/main.rs
+++ 03-podatkovni-tipi/structs/src/main.rs
@@ -1,25 +1,109 @@
-struct AritmeticnoZaporedje {
-    zacetni_clen: u32,
-    razlika: u32,
-}
 use std::{
     iter::Sum,
     ops::{Add, Mul, Sub},
 };
 
+#[derive(Copy)]
+struct AritmeticnoZaporedje<T> {
+    zacetni_clen: T,
+    razlika: T,
+}
+
 use AritmeticnoZaporedje as A;
 
-impl A {
-    fn new(a: u32, d: u32) -> A {
+impl<T> A<T> {
+    fn new(a: T, d: T) -> A<T> {
         return A {
             zacetni_clen: a,
             razlika: d,
         };
     }
-
-    //fn n_th(zap: A, n: u32) -> u32 {}
 }
-// tole zgorej je zalost, da ni narejeno
+
+impl<T> A<T>
+where
+    T: Sum<T> + Copy + Add<Output = T> + Mul<Output = T> + From<u32>,
+{
+    fn next(&mut self) -> T {
+        let clen = self.zacetni_clen;
+        self.zacetni_clen = self.zacetni_clen + self.razlika;
+        clen
+    }
+
+    fn n_th(&self, n: u32) -> T {
+        self.zacetni_clen + self.razlika * T::from(n - 1)
+    }
+
+    fn sum(&self, n: u32) -> T {
+        let mut vsota = self.zacetni_clen;
+        for i in 2..=n {
+            vsota = vsota + self.n_th(i);
+        }
+        vsota
+    }
+}
+trait Zaporedje<T> {
+    fn name(&self) -> String;
+    fn k_th(&self, k: u32) -> Option<T>;
+    fn contains(&self, x: T) -> bool;
+    fn start(&self) -> T;
+}
+
+struct Constant<T> {
+    c: T,
+}
+
+impl<T> Constant<T> {
+    fn new(c: T) -> Constant<T> {
+        Constant { c }
+    }
+}
+
+// impl<T> Zaporedje<T> for Constant<T> {
+//     fn name(&self) -> String {
+//         "Konstantno zaporedje".to_string()
+//     }
+
+//     fn k_th(&self, k: u32) -> Option<T> {
+//         Some(self.c)
+//     }
+
+//     fn contains(&self, x: T) -> bool {
+//         self.c == x
+//     }
+
+//     fn start(&self) -> T {
+//         self.c
+//     }
+// }
+
+impl Zaporedje<i64> for Constant<i64> {
+    fn name(&self) -> String {
+        "Konstantno zaporedje".to_string()
+    }
+
+    fn k_th(&self, k: u32) -> Option<i64> {
+        Some(self.c)
+    }
+
+    fn contains(&self, x: i64) -> bool {
+        self.c == x
+    }
+
+    fn start(&self) -> i64 {
+        self.c
+    }
+}
+
+struct Const {
+    c: i64,
+}
+
+impl Const {
+    fn new(c: i64) -> Const {
+        Const { c }
+    }
+}
 
 //naredimo AST
 #[derive(Copy, Clone)]
